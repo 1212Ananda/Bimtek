d6681968016536ed86425e6eb6fd6a0ec966f464
unknown <1212Ananda>
2024-01-12 12:21:29 +0700

    bimtek_BBKKP

---
+++ resources/views/layouts/landingpage.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+  
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container">
+        <img src="../img/logobbkkp.jpeg"  style="max-width: 150px; height: auto;" alt="">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+              
+              <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-houses-fill"></i> Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-currents="page" href="#"><i class="bi bi-pencil-square"></i> Pendaftaran</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-currents="page" href="#"><i class="bi bi-person-lines-fill"></i> Kontak</a>
+            </li>
+          </ul>
+          <i class="bi bi-box-arrow-in-right"></i>
+        </div>
+      </div>
+    </nav>
+    <header class="hero-section">
+      <div class="container">
+          <div class="row align-items-center">
+              <div class="col-lg-6">
+                  <h1 class="display-4">Welcome to Your Website</h1>
+                  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+                  <a href="#" class="btn btn-primary btn-lg">Get Started</a>
+              </div>
+              <div class="col-lg-6">
+                  <!-- Your Hero Image -->
+                  <img src="path/to/your/image.jpg" alt="Hero Image" class="img-fluid">
+              </div>
+          </div>
+      </div>
+      
+  </header>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+  </body>
+</html>
